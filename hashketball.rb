0716f4d87900c3f82ed3c1af66f797a9dfc66cba
Conor Murnane <conormurnane11@gmail.com>
2020-11-08 17:36:46 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,12 @@
 # Write your code below game_hash
+require 'pry'
 def game_hash
   {
     home: {
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
       players: [
-        {
+        { 
           player_name: "Alan Anderson",
           number: 0,
           shoe: 16,
@@ -126,4 +127,218 @@ def game_hash
   }
 end
 
+
 # Write code here
+def num_points_scored(player_name)
+#  binding.pry
+#new_hash = {}
+    game_hash.each do |key,value|
+        #key = home or away
+        #value = entire data set within it
+          value.each do |inner_key,inner_value|
+         #inner_key = :Team_name
+         
+                if inner_key == :players
+                 # binding.pry
+                      inner_value.each do |stat_key,stat_value|
+                       # binding.pry
+                          if stat_key[:player_name] == player_name
+                       # binding.pry
+                         
+         
+                          return stat_key[:points]
+                         # binding.pry
+                          end
+                       #binding.pry
+                       end
+               
+            end
+       
+        end
+
+   end
+        
+end
+
+def shoe_size(player_name)
+    game_hash.each do |key,value|
+        #key = home or away
+        #value = entire data set within it
+          value.each do |inner_key,inner_value|
+         #inner_key = :Team_name
+         
+                if inner_key == :players
+                 # binding.pry
+                      inner_value.each do |stat_key,stat_value|
+                      #  binding.pry
+                          if stat_key[:player_name] == player_name
+                       # binding.pry
+                         
+         
+                          return stat_key[:shoe]
+                         # binding.pry
+                          end
+                       #binding.pry
+                       end
+               
+            end
+       
+        end
+
+   end
+
+end
+
+
+
+def team_colors(team)
+        game_hash.each do |key,value|
+        #key = home or away
+        #value = entire data set within it
+          value.each do |inner_key,inner_value|
+         #inner_key = :Team_name
+        # binding.pry
+                if inner_value == team
+                 # binding.pry
+                 return value[:colors]
+              
+               
+            end
+       
+        end
+
+   end
+end
+
+def team_names
+   teams = []
+    game_hash.each do |key,value|
+       # binding.pry
+        #key = home or away
+        #value = entire data set within it
+          value.each do |inner_key,inner_value|
+         #inner_key = :Team_name
+        # binding.pry
+       if inner_value == value[:team_name]
+         #binding.pry
+         teams << inner_value
+         #binding.pry
+         teams
+       end
+        end
+     # binding.pry 
+      teams
+     #binding.pry
+   end
+   #binding.pry
+teams
+end
+
+def player_numbers(team_name)
+      jersey_numbers = []
+      game_hash.each do |key,value|
+        #key = home or away
+        #value = entire data set within it
+        #binding.pry
+         # value == :team_name
+            #binding.pry
+          if value[:team_name] == team_name
+            
+               value.each do |inner_key,inner_value|
+                #binding.pry            
+                if inner_key == :players 
+                 #binding.pry
+                      inner_value.each do |stat_key,stat_value|
+                      #  binding.pry
+                        #if stat_value == :number
+                         # binding.pry
+                        jersey_numbers << stat_key[:number]
+                      #  binding.pry
+                        jersey_numbers
+                       # binding.pry
+                      #end
+                  end
+                  # jersey_numbers
+                 #binding.pry
+                    end
+                    #jersey_numbers
+                   # binding.pry
+                  end
+                  
+                  #jersey_numbers
+                 # binding.pry
+            end
+            #jersey_numbers
+            #binding.pry
+
+
+    end
+    jersey_numbers
+    #binding.pry
+end
+
+def player_stats(player_name)
+      game_hash.each do |key,value|
+        #key = home or away
+        #value = entire data set within it
+          value.each do |inner_key,inner_value|
+         #inner_key = :Team_name
+         
+                if inner_key == :players
+                 # binding.pry
+                      inner_value.each do |stat_key,stat_value|
+                      #  binding.pry
+                          if stat_key[:player_name] == player_name
+                       # binding.pry
+                         #binding.pry
+         
+                          return stat_key
+                         # binding.pry
+                          end
+                       #binding.pry
+                       end
+               
+            end
+       
+        end
+
+   end
+
+end
+
+def big_shoe_rebounds
+  rebounds_shoe_size = 4
+   largest_shoe_size = 4
+      game_hash.each do |key,value|
+        #key = home or away
+        #value = entire data set within it
+          value.each do |inner_key,inner_value|
+         #inner_key = :Team_name
+        
+        # rebounds_shoe_size = 0
+                if inner_key == :players
+                 # binding.pry
+                      inner_value.each do |stat_key,stat_value|
+                       # binding.pry
+                          if stat_key[:shoe] >  largest_shoe_size
+                               # binding.pry
+                              largest_shoe_size = stat_key[:shoe]
+                                #binding.pry
+                                
+                               rebounds_shoe_size = stat_key[:rebounds]
+                                 # binding.pry
+                                 #rebounds_shoe_size
+                                 
+                          end
+                      # binding.pry
+                       #return stat_key[:rebounds]
+                     #  rebounds_shoe_size
+                       end
+            end
+            #binding.pry
+     # rebounds_shoe_size
+        end
+   end
+rebounds_shoe_size
+
+end
